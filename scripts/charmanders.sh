fd70ac89b4b1ee4b6aedd86edca4fd02a5cc224b
Richard Wu <rwu@palantir.com>
2016-07-22 14:42:06 -0700

    wip

---
+++ scripts/charmanders.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python runserver.py -a ptc -u pokemaphax -p lapras -l "37.457664,-122.187931" -st 7
---
+++ scripts/menlopark.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python runserver.py -a ptc -u pokemaphax -p lapras -l "37.4576556,-122.1901095" -st 10
